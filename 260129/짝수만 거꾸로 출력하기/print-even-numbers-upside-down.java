9d3ecc59a23264cf7e1961523ea6fa6d531fbd53
2020147536 <2001jjmm@naver.com>
2026-01-29 16:50:55 +0900

    Title: 짝수만 거꾸로 출력하기, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 260129/짝수만 거꾸로 출력하기/print-even-numbers-upside-down.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        // Please write your code here.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Stack<Integer> s = new Stack<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int a = Integer.parseInt(st.nextToken());
+            if (a%2 == 0) s.push(a);
+        }
+        StringBuilder sb = new Stringbuilder();
+        while (!s.isEmpty()) {
+            sb.append(s.pop()).append(" ");
+        }
+        System.out.println(sb);
+    }
+}
