4c7a7f9c762514f5b6c0b778dc3fd366659afb09
2020147536 <2001jjmm@naver.com>
2026-01-29 11:16:41 +0900

    Title: B부터 A까지 감소, Time: 125ms, Memory: 10MB, Status: Passed - Codetree

--- 260129/B부터 A까지 감소/dec-from-b-to-a.java
+++ 260129/B부터 A까지 감소/dec-from-b-to-a.java
@@ -8,7 +8,7 @@ public class Main {
         int b = sc.nextInt();
 
         for (int i = b; i >= a; i--) {
-            System.out.println(i + " ");
+            System.out.print(i + " ");
         }
     }
 }
