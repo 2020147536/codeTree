32b5375a6359f1e645bc64df071e3f3e264ed712
2020147536 <2001jjmm@naver.com>
2026-01-29 09:00:29 +0900

    Title: 입력받은 값과 합 출력, Time: 92ms, Memory: 9MB, Status: Passed - Codetree

--- 260129/입력받은 값과 합 출력/output-of-value-and-sum.java
+++ 260129/입력받은 값과 합 출력/output-of-value-and-sum.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
         // Please write your code here.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
