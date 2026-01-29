ce2a88d3edf808b8df6646103aa2bec900530930
2020147536 <2001jjmm@naver.com>
2026-01-29 16:00:05 +0900

    Title: 남녀노소 구분짓기, Time: 123ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260129/남녀노소 구분짓기/sex-and-age.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int g = sc.nextInt();
+        int a = sc.nextInt();
+        if (g == 0) {
+            if (a >= 19) System.out.println("MAN");
+            else System.out.println("BOY");
+        } else {
+            if (a >= 19) System.out.println("WOMAN");
+            else System.out.println("GIRL");
+        }
+    }
+}
