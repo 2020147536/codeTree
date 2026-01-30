983e8c5c636d6af320c2ecd09be8429e75d66746
2020147536 <2001jjmm@naver.com>
2026-01-30 09:41:19 +0900

    Title: 상하로 반복하며 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

--- 260130/상하로 반복하며 출력/print-in-up-down-reverse.java
+++ 260130/상하로 반복하며 출력/print-in-up-down-reverse.java
@@ -6,9 +6,9 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[][] map = new int[n][n];
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                map[j][i] = j;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                map[j][i] = j+1;
             }
         }
          for (int i = 0; i < n; i++) {
