05629b2a121afefede3af7a8ab46e3235fefc1d2
2020147536 <2001jjmm@naver.com>
2026-01-30 09:25:44 +0900

    Title: 5의 배수의 개수는?, Time: 126ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260130/5의 배수의 개수는?/the-number-of-multiples-of-5.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int cnt = 0;
+        for (int i = 0; i < 16; i++) {
+            if (sc.nextInt()%5==0) cnt++;
+        }
+        System.out.print(cnt);
+    }
+}
