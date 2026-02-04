12fdf733162d2d29b8d1027a0232d45f7a9b5373
jjmm0705 <2001jjmm@naver.com>
2026-02-04 14:52:43 +0900

    Title: 변수 출력하기, Time: 91ms, Memory: 9MB, Status: Passed - Codetree

--- 260204/변수 출력하기/outputing-variables.java
+++ 260204/변수 출력하기/outputing-variables.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         // Please write your code here.
         int a = 3;
-        char b = C;
+        char b = 'C';
         System.out.printf("%d...%c", a, b);
     }
 }
